c45fa3a08b1ea777f64cb04ed64b1a34833df195
MinWenaaa <18327210767@163.com>
2024-10-20 12:41:03 +0800

    解析法相对定位

--- spatial_rear_intersection.py
+++ spatial_rear_intersection.py
@@ -71,15 +71,3 @@ def spatialRearIntersection(m, f, image, ground):
 
     return np.hstack((X, VTPV))
 
-
-
-
-m = float(50000)
-f = float(153.24)
-path = 'input1.xlsx'
-df = pd.read_excel(path, engine='openpyxl', dtype=float)
-coordination = df.to_numpy()
-
-X = spatialRearIntersection(m, f, coordination[:,0:2], coordination[:,2:5])
-
-
